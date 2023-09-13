58a297744acd54d67d2f9968d7703b426efbf4e5
Vagner Alexander <onemybox@gmail.com>
2023-09-13 12:45:36 +0300

    функция-предикат

---
+++ 5_predicate_function.fs
@@ -0,0 +1,14 @@
+// 16.1
+let notDivisible(n, m) = n % m = 0 // int * int -> bool
+
+// 16.2
+let prime n = // int -> bool
+
+    let maxDivider = int(sqrt(float(n)))
+
+    let rec notDiv(n, m) =
+        match m with
+        | 0 | 1 -> true
+        | _ -> (n % m <> 0) && notDiv (n, m - 1)
+    
+    notDiv(n, maxDivider)
