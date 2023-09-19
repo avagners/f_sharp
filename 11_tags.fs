a5ba112fdaa5cd98d8e404e5e1c2b3cd9277ddb4
Vagner Alexander <onemybox@gmail.com>
2023-09-19 23:42:45 +0300

    Размеченные объединения

--- 11_tags.fs
+++ 11_tags.fs
@@ -6,4 +6,5 @@ type TimeOfDay = { hours : int; minutes : int; f: F }
 
 let (.>.) (x: TimeOfDay) (y: TimeOfDay) = // TimeOfDay -> TimeOfDay -> bool
     if x.f > y.f then true
+    elif x.f < y.f then false
     else x > y
