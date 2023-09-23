726d8140aba0269a231398847f82069cce64ddab
Vagner Alexander <onemybox@gmail.com>
2023-09-23 08:24:45 +0300

    Функции свёртки fold и foldBack

--- 15_hof_lists.fs
+++ 15_hof_lists.fs
@@ -5,7 +5,7 @@ let list_filter f (xs: 'a list) : 'a list = // ('a -> bool) -> 'a list -> 'a lis
     List.foldBack _filter xs []
 
 // 41.4.2
-let sum (p, xs: int list) = // (int -> bool) * int list -> int
+let sum (p, xs: int list) : int = // (int -> bool) * int list -> int
     let _sum (acc: int) (item: int) : int = if p item then item + acc 
                                             else acc
     List.fold _sum 0 xs
