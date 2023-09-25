ebacba0f2f2a79c1ed9265a7c1acc82da6cc8073
Vagner Alexander <onemybox@gmail.com>
2023-09-25 10:34:48 +0300

    Императивное программирование

--- 18_imperative_style.fs
+++ 18_imperative_style.fs
@@ -12,13 +12,13 @@ let f (n: int) : int =
 // 47.4.2
 let mutable previous: int = 0
 let mutable current: int = 1
-let mutable counter: int = 1
+let mutable counter2: int = 1
 
 let fibo (n: int) : int = // int -> int
     if n = 0 then 0 else
-    while counter < n do
+    while counter2 < n do
         let next: int = previous + current
         previous <- current
         current <- next
-        counter <- counter + 1
+        counter2 <- counter2 + 1
     current
