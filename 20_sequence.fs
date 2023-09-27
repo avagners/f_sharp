205715049e94390702457b130d3ebb226ebd0fe4
Vagner Alexander <onemybox@gmail.com>
2023-09-27 14:20:17 +0300

    Ленивые последовательности

--- 20_sequence.fs
+++ 20_sequence.fs
@@ -1,19 +1,19 @@
 // 49.5.1
 let sqr: int seq = // seq<int>
-    Seq.initInfinite ( function (i: int) -> (i + 1) * 2 )
+    Seq.initInfinite ( function n -> (n + 1) * 2 )
 
 // 49.5.2
 let rec factorial = function // int -> int
     | 0 -> 1
-    | (n: int) -> n * factorial (n - 1)
+    | n -> n * factorial (n - 1)
 
-let fac_seq: int seq = Seq.initInfinite factorial // seq<int>
+let fac_seq: int seq = Seq.initInfinite (factorial) // seq<int>
 
 // 49.5.3
 let seq_seq: int seq = // seq<int>
     
     let _f = function
-        | (n: int) when n % 2 = 0 -> n / 2 
-        | (n: int) -> -(n + 1) / 2
+        | n when n % 2 = 0 -> n / 2 
+        | n -> -1 *(n + 1) / 2
     
-    Seq.initInfinite _f
+    Seq.initInfinite (_f)
